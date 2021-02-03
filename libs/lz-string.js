6f567c28ee1d5a1cb8d71d6d3c6bf00a025845e6
starshine <th.starshine@gmail.com>
2021-02-03 14:23:29 +0800

    chore: export es6 module

--- libs/lz-string.js
+++ libs/lz-string.js
@@ -494,13 +494,4 @@ var LZString = {
   return LZString;
 })();
 
-if (typeof define === 'function' && define.amd) {
-  define(function () { return LZString; });
-} else if( typeof module !== 'undefined' && module != null ) {
-  module.exports = LZString
-} else if( typeof angular !== 'undefined' && angular != null ) {
-  angular.module('LZString', [])
-  .factory('LZString', function () {
-    return LZString;
-  });
-}
+export default LZString;
